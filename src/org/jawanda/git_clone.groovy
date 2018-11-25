3ac2546a1aa26972d0f4816f78f2c301d09c940b
varinderjawanda <anyforms01@gmail.com>
2018-11-25 14:01:48 -0500

    Update git_clone.groovy

--- src/org/jawanda/git_clone.groovy
+++ src/org/jawanda/git_clone.groovy
@@ -1,5 +1,6 @@
 package org.jawanda
 def checkOutFrom(repo) {
-  git url: "git@github.com:varinderjawanda/${repo}"
+  echo "from res - Cloning GIT URL: ${repo}"
+  //git url: "git@github.com:varinderjawanda/${repo}"
 }
 return this
