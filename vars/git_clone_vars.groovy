4d2826976dc5165f5cc6b05f5aae706565fa8e97
varinderjawanda <anyforms01@gmail.com>
2018-12-28 12:42:29 -0500

    Update git_clone_vars.groovy

--- vars/git_clone_vars.groovy
+++ vars/git_clone_vars.groovy
@@ -1,5 +1,5 @@
 def url (repo) {
   def GIT_URL = "https://github.com/varinderjawanda/${repo}"
   echo "Cloning GIT URL: ${GIT_URL}"
-  sh "git clone ${GIT_URL} && cd ${repo} && ls -lrt"
+  sh "rm -rf ${repo} && git clone ${GIT_URL} && cd ${repo} && ls -lrt"
 }
